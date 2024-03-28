d9f4e3bad13c939d3649edc7d763c9bef78e8437
yash-avasekar <stevefrayne3@gmail.com>
2024-03-28 11:38:44 +0530

    added Comment Model

--- blog_app/Comment/models.py
+++ blog_app/Comment/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from Post.models import Post
+from Profile.models import Profile
 
 # Create your models here.
+
+
+# Comment
+class Comment(models.Model):
+    """
+    Comment
+    - comment on post, comment by profile( user )
+    - comment text, commented_on
+    """
+
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_text = models.TextField()
+    commented_on = models.DateTimeField(auto_now_add=True)
